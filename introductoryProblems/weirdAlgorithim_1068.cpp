d974c5c05b1be4234ee2e4c5e192695d652b0762
nisterqwa <nisterqwa@gmail.com>
2026-03-15 21:32:32 +0100

    Final solution

--- introductoryProblems/weirdAlgorithim_1068.cpp
+++ introductoryProblems/weirdAlgorithim_1068.cpp
@@ -3,20 +3,19 @@
 #include <vector>
 
 int main(){
-    int n;
+    long long n;
     std::cin >> n;
-    while (n > 1)
+    std::cout << n << std::endl;
+    while (n >1)
     {
         if (n % 2 == 0)
         {
-           n = n / 2;
-
+            n = n / 2;
         }
         else if (n % 2 == 1)
         {
             n = (n * 3) + 1;
         }
-
-        std::cout << n << std::endl;
+         std::cout << n << " ";
     }
 }
